c484cfc47d2802bec9aa135ce18121acaf280175
ArthurClaro <0123456789arthur@gmail.com>
2024-05-29 12:11:13 -0300

    feat:

--- Front-End/src/services/api.js
+++ Front-End/src/services/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL: 'https://m6-fullstack.onrender.com/',
-    // baseURL: "https://fruit-fake-api.onrender.com/",
-
-    // baseURL: 'http://localhost:5173/',
-    //  <--- Rodar local : PORT : 3000
-    
-    // baseURL: 'https://metafisic.onrender.com/',
-    baseURL: 'http://localhost:3000/',
-
+    baseURL: 'https://metafisic.onrender.com/',
+    // vercel --prod
+    // baseURL: 'http://localhost:3000/',
     timeout: 10000,
 })
+
